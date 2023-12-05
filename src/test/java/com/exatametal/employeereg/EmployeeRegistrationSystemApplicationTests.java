540308e09db214f920627e692bcacf56b67e2aa7
Gustavo Morais <gustvmorais@gmail.com>
2023-12-05 10:48:03 -0300

    First commit

---
+++ src/test/java/com/exatametal/employeereg/EmployeeRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exatametal.employeereg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegistrationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
